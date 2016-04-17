d9d42ab105e09911eea0d73ee68fe3364ab3b077
YuanYuan <yuan.tops@gmail.com>
2016-04-17 17:46:29 +0800

    configure org-mode

--- init.el
+++ init.el
@@ -4,14 +4,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(package-archives
+ '(org-agenda-files (quote ("~/Personal/Notes/worklog_daily_工作笔记.org")))
+ '(org-startup-truncated nil)
+ '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "http://stable.melpa.org/packages/")))))
+     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+ '(truncate-lines nil))
 
 ;; show line number
 (global-linum-mode t)
 
+;; add config file load path
 (add-to-list 'load-path "~/.emacs.d/src/")
 
 ;; markdown mode
@@ -32,3 +36,22 @@
    (setq chinese-font-size-scale-alist '((11.5 . 1.25) (16 . 1.25))))
   (t ;; is a linux
     (setq chinese-font-size-scale-alist '((16 . 1.25)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+ ;; org-mode key bindings
+ (global-set-key "\C-cl" 'org-store-link)
+ (global-set-key "\C-ca" 'org-agenda)
+ (global-set-key "\C-cc" 'org-capture)
+ (global-set-key "\C-cb" 'org-iswitchb)
+
+;; org-mode: syntax highlight for src code blocks
+(setq org-src-fontify-natively t)
+
+;; org-mode: enable markdown exporter
+(eval-after-load "org"
+  '(require 'ox-md nil t))
